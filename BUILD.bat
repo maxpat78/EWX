670dc1a1f6f312f99422ee81f08baf9f0de672ec
maxpat78 <maxpat78@yahoo.it>
2022-10-26 16:34:23 +0200

    more EWX_xxx flags added
    Added options to reflect new Windows XP+ flags

--- BUILD.bat
+++ BUILD.bat
@@ -1,13 +1,13 @@
 @echo off
 echo Compilazione di EWX (utilità ExitWindowsEx) per Windows NT/XP...
-cl -nologo -FeEWX_NT.exe -O1g -DMADE_FOR_NT EWX32.c
+cl -nologo -FeEWX_NT.exe -O1s -DMADE_FOR_NT EWX32.c
 echo Compilazione di EWX (utilità ExitWindowsEx) per Windows 9x...
-cl -nologo -FeEWX_9x.exe -O1g EWX32.c EWX16.cpp
+cl -nologo -FeEWX_9x.exe -O1s EWX32.c EWX16.cpp
 echo Compilazione di EWX (utilità ExitWindowsEx) per Windows Vista+...
-cl -nologo -FeEWX_NEW.exe -O1g EWX32_NEW.c
+cl -nologo -FeEWX_NEW.exe -O1s EWX32_NEW.c
 del *.obj
 echo.
-echo Uso: EWX -logoff/-reboot/-poweroff/-shutdown/-force
+echo Uso: EWX [-5] -logoff/-reboot/-poweroff/-shutdown/-force
 echo          [-restart/-suspend/-exit (Win9x)]
 echo Senza parametri, non esegue alcuna azione.
 echo.
--- EWX32_NEW.c
+++ EWX32_NEW.c
@@ -1,67 +1,74 @@
-/* Mini utilit… per disconnetersi, spegnere e riavviare semplicemente */
+ï»¿#pragma comment(linker,"/DEFAULTLIB:ADVAPI32.lib")
+#pragma comment(linker,"/DEFAULTLIB:KERNEL32.lib")
+#pragma comment(linker,"/DEFAULTLIB:POWRPROF.lib")
+#pragma comment(linker,"/DEFAULTLIB:SHELL32.lib")
+#pragma comment(linker,"/DEFAULTLIB:SHLWAPI.lib")
+#pragma comment(linker,"/DEFAULTLIB:USER32.lib")
 
 #pragma comment(linker,"/ENTRY:EWX")
 #pragma comment(linker,"/SUBSYSTEM:windows")
-#pragma comment(linker,"/RELEASE")
 
-#pragma comment(linker,"/DEFAULTLIB:ADVAPI32.lib")
-#pragma comment(linker,"/DEFAULTLIB:USER32.lib")
-#pragma comment(linker,"/DEFAULTLIB:KERNEL32.lib")
-#pragma comment(linker,"/DEFAULTLIB:SHLWAPI.lib")
-#pragma comment(linker,"/DEFAULTLIB:POWRPROF.lib")
-#define strstr StrStr
-
-#define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <shlwapi.h>
 #include <powrprof.h>
 
-/* Su NT bisogna essere "privilegiati" per chiudere la sessione... */
-BOOL NT_Set_Privilege()
-{
- HANDLE hToken; 
- LUID ShutdownNameValue; 
- TOKEN_PRIVILEGES tkp; 
- 
- if (!OpenProcessToken(GetCurrentProcess(),TOKEN_ADJUST_PRIVILEGES|TOKEN_QUERY,&hToken)) return 0;
- if (!LookupPrivilegeValue((LPSTR)0,SE_SHUTDOWN_NAME,&ShutdownNameValue)) return 0;
- 
- tkp.PrivilegeCount = 1; 
- tkp.Privileges[0].Luid = ShutdownNameValue; 
- tkp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED; 
+BOOL NT_Set_Privilege() {
+	HANDLE hToken; 
+	LUID ShutdownNameValue; 
+	TOKEN_PRIVILEGES tkp; 
 
- AdjustTokenPrivileges(hToken,0,&tkp,sizeof(TOKEN_PRIVILEGES),(PTOKEN_PRIVILEGES)0,(PDWORD)0);
- if (GetLastError() != ERROR_SUCCESS) return 0; 
- 
- return 1;
+	if (!OpenProcessToken(GetCurrentProcess(),TOKEN_ADJUST_PRIVILEGES|TOKEN_QUERY,&hToken)) return 0;
+	if (!LookupPrivilegeValue((LPSTR)0,SE_SHUTDOWN_NAME,&ShutdownNameValue)) return 0;
+
+	tkp.PrivilegeCount = 1; 
+	tkp.Privileges[0].Luid = ShutdownNameValue; 
+	tkp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED; 
+
+	AdjustTokenPrivileges(hToken,0,&tkp,sizeof(TOKEN_PRIVILEGES),(PTOKEN_PRIVILEGES)0,(PDWORD)0);
+	if (GetLastError() != ERROR_SUCCESS) return 0;  
+
+	return 1;
 }
 
-BOOL EWX()
-{
-	int op = 0;
-	if ( strstr(GetCommandLine(),"-logoff") )
-		op |= EWX_LOGOFF;
-	if ( strstr(GetCommandLine(),"-force") )
-		op |= EWX_FORCE;
-/* Mentre su NT EWX_POWEROFF fa proprio quel che promette, su Win98 no!*/
-/* Ivi solo EWX_SHUTDOWN spegne il sistema togliendo la corrente. */
-	if ( strstr(GetCommandLine(),"-poweroff") )
-		op |= EWX_POWEROFF;
-	if ( strstr(GetCommandLine(),"-shutdown") )
-		op |= EWX_SHUTDOWN;
-	if ( strstr(GetCommandLine(),"-reboot") )
-		op |= EWX_REBOOT;
-/* Chiude la Barra delle Applicazioni (Explorer): si guadagna RAM? */
-	if ( strstr(GetCommandLine(),"-exit") )
+BOOL EWX() {
+	UINT op = 0;
+	char *s = GetCommandLine();
+
+	if ( StrStr(s,"-exit") )
 		return PostMessage(FindWindow("Progman","Program Manager"),WM_QUIT,0,0);
-	if ( strstr(GetCommandLine(),"-suspend") )
-		return SetSuspendState(0, 0, 0);
-	if ( strstr(GetCommandLine(),"-hibernate") )
+
+	if ( StrStr(s,"-force") )
+		op |= EWX_FORCE;
+
+	if ( StrStr(s,"-forceifhung") )
+		op |= EWX_FORCEIFHUNG;
+
+	if ( StrStr(s,"-hibernate") )
 		return SetSuspendState(1, 0, 0);
 
-	if ( !(op&EWX_LOGOFF) && !NT_Set_Privilege())
-		return MessageBox(0,"Mancano i necessari privilegi.","Impossibile!",MB_ICONSTOP);
+	if ( StrStr(s,"-hybrid") )
+		op |= EWX_HYBRID_SHUTDOWN;
 
-/* Senza parametri, non succede altro! */
+	if ( StrStr(s,"-logoff") )
+		op |= EWX_LOGOFF;
+
+	if ( StrStr(s,"-poweroff") )
+		op |= EWX_POWEROFF;
+
+	if ( StrStr(s,"-reboot") )
+		op |= EWX_REBOOT;
+
+	if ( StrStr(s,"-restartapps") )
+		op |= EWX_RESTARTAPPS;
+
+	if ( StrStr(s,"-shutdown") )
+		op |= EWX_SHUTDOWN;
+
+	if ( StrStr(s,"-suspend") )
+		return SetSuspendState(0, 0, 0);
+	
+	if ( !(op&EWX_LOGOFF) && !NT_Set_Privilege())
+		return MessageBox(0,"ShutdownNameValue privilege not held!","Can't do that...",MB_ICONSTOP);
+
 	return ExitWindowsEx(op,0);
 } 
